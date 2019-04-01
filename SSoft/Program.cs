7f9fafa70e7a1645e05b77ef45e1bb926605418e
JakeSmokie <jakesmokie@gmail.com>
2019-04-01 16:03:17 +0300

    Add project files.

---
+++ SSoft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSoft {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
